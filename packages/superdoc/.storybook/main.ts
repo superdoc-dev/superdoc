cb326f2dbdcbf2562232842e1504043b3acc7189
Tony Novak <tony.novak@harbourshare.com>
2025-12-02 00:23:04 -0500

    feat: remove @storybook/addon-onboarding

--- packages/superdoc/.storybook/main.ts
+++ packages/superdoc/.storybook/main.ts
@@ -21,7 +21,6 @@ const config: StorybookConfig = {
     getAbsolutePath('@storybook/addon-vitest'),
     getAbsolutePath('@storybook/addon-a11y'),
     getAbsolutePath('@storybook/addon-docs'),
-    getAbsolutePath('@storybook/addon-onboarding')
   ],
   "framework": getAbsolutePath('@storybook/vue3-vite'),
   "staticDirs": [

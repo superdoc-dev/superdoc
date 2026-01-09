31208f537b46ca54fc3774753056c13d7f8ee761
Luccas Correa <luccas@superdoc.dev>
2026-01-09 19:23:39 -0300

    feat: add v3 translator for w:abstractNum

--- packages/super-editor/src/core/super-converter/v3/handlers/index.js
+++ packages/super-editor/src/core/super-converter/v3/handlers/index.js
@@ -6,6 +6,7 @@ import { translator as sd_index_translator } from './sd/index/index-translator.j
 import { translator as sd_indexEntry_translator } from './sd/indexEntry/indexEntry-translator.js';
 import { translator as sd_autoPageNumber_translator } from './sd/autoPageNumber/autoPageNumber-translator.js';
 import { translator as sd_totalPageNumber_translator } from './sd/totalPageNumber/totalPageNumber-translator.js';
+import { translator as w_abstractNum_translator } from './w/abstractNum/abstractNum-translator.js';
 import { translator as w_abstractNumId_translator } from './w/abstractNumId/abstractNumId-translator.js';
 import { translator as w_adjustRightInd_translator } from './w/adjustRightInd/adjustRightInd-translator.js';
 import { translator as w_autoRedefine_translator } from './w/autoRedefine/autoRedefine-translator.js';
@@ -204,6 +205,7 @@ const translatorList = Array.from(
     sd_indexEntry_translator,
     sd_autoPageNumber_translator,
     sd_totalPageNumber_translator,
+    w_abstractNum_translator,
     w_abstractNumId_translator,
     w_adjustRightInd_translator,
     w_autoRedefine_translator,

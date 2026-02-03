ea36b550cc0986e461fdbd1ea5ed95374a573c09
Tadeu Tupinamba <tadeu.tupiz@gmail.com>
2026-02-03 16:55:26 -0300

    chore(react): remove temporary release branch config

--- packages/react/.releaserc.cjs
+++ packages/react/.releaserc.cjs
@@ -5,8 +5,6 @@ const config = {
   branches: [
     { name: 'stable', channel: 'latest' },
     { name: 'main', prerelease: 'next', channel: 'next' },
-    // TEMPORARY: Remove before merging to main
-    { name: 'feat/react-wrapper', prerelease: 'canary', channel: 'canary' },
   ],
   tagFormat: 'react-v${version}',
   plugins: [

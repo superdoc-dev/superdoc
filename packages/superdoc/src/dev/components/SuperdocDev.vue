79be1a348aecf2cfe1a052d8e90319221e9fbf74
aorlov <andrii@harbourshare.com>
2026-02-06 22:58:08 +0100

    fix: disable telemetry by default

--- packages/superdoc/src/dev/components/SuperdocDev.vue
+++ packages/superdoc/src/dev/components/SuperdocDev.vue
@@ -180,7 +180,7 @@ const init = async () => {
     documentMode: 'editing',
     licenseKey: 'community-and-eval-agplv3',
     telemetry: {
-      enabled: true
+      enabled: false,
     },
     comments: {
       visible: true,

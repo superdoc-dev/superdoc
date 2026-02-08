1950d2e0125a6994140dd0c577042629bc67357c
Nick Bernal <nick@superdoc.dev>
2026-02-08 10:51:06 -0800

    chore: fix mount Vue on wrapper element and fix React docs lifecycle cleanup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,7 @@ export default [
   {
     ignores: [
       '**/dist/**',
+      '**/dist-types/**',
       '**/node_modules/**',
       // Generated/vendor files that shouldn't be linted
       '**/pdfjs.js',

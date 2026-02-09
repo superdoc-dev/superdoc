f81558dae3bed28fa0297bc68f015a9ee2181653
Luccas Correa <luccas@harbourshare.com>
2026-02-09 15:43:45 -0300

    feat: convert inline permission nodes to block ones when under root node

--- packages/super-editor/src/core/super-converter/v2/importer/docxImporter.js
+++ packages/super-editor/src/core/super-converter/v2/importer/docxImporter.js
@@ -802,6 +802,8 @@ export function filterOutRootInlineNodes(content = []) {
     'commentReference',
     'footnoteReference',
     'structuredContent',
+    'permStart',
+    'permEnd',
   ]);
 
   const PRESERVABLE_INLINE_XML_NAMES = {
@@ -823,6 +825,14 @@ export function filterOutRootInlineNodes(content = []) {
         content: [node],
         attrs: {},
         marks: [],
+      });
+      return;
+    }
+
+    if (type === 'permStart' || type === 'permEnd') {
+      result.push({
+        ...node,
+        type: type === 'permStart' ? 'permStartBlock' : 'permEndBlock',
       });
       return;
     }

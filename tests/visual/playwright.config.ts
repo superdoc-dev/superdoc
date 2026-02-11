45f2a524a0eb6f8e97a51d153dbd1cf576248f01
Caio Pizzol <caio@harbourshare.com>
2026-02-11 18:32:56 -0300

    perf(visual-testing): increase Playwright workers to 4

--- tests/visual/playwright.config.ts
+++ tests/visual/playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  workers: 1,
+  workers: 4,
   reporter: [['html', { open: 'never' }]],
 
   expect: {

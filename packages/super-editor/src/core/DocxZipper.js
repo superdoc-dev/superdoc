fb320d65e966d978d1563b7fb0bedb2e80c0475a
Caio Pizzol <caio@harbourshare.com>
2026-02-14 09:51:21 -0300

    fix: replace Node.js Buffer APIs with browser-native alternatives
    Buffer.from, Buffer.isBuffer, and buffer-crc32 crash in browser/CDN
    environments. Use Uint8Array, ArrayBuffer.isView, TextEncoder, and a
    custom CRC32 table instead.

--- packages/super-editor/src/core/DocxZipper.js
+++ packages/super-editor/src/core/DocxZipper.js
@@ -1,6 +1,6 @@
 import * as xmljs from 'xml-js';
 import JSZip from 'jszip';
-import { getContentTypesFromXml } from './super-converter/helpers.js';
+import { getContentTypesFromXml, base64ToUint8Array } from './super-converter/helpers.js';
 import { ensureXmlString, isXmlLike } from './encoding-helpers.js';
 
 /**
@@ -303,7 +303,7 @@ class DocxZipper {
     });
 
     Object.keys(media).forEach((path) => {
-      const binaryData = Buffer.from(media[path], 'base64');
+      const binaryData = base64ToUint8Array(media[path]);
       zip.file(path, binaryData);
     });
 

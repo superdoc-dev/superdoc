4f052dbb45046faa7b28f44b003998798cf82495
aorlov <andrii@harbourshare.com>
2026-02-19 23:36:23 +0100

    fix: update telemetry source to 'template-builder-demo'

--- packages/template-builder/demo/src/App.tsx
+++ packages/template-builder/demo/src/App.tsx
@@ -251,7 +251,7 @@ export function App() {
           telemetry={{
             enabled: true,
             metadata: {
-              source: 'template-builder',
+              source: 'template-builder-demo',
             },
           }}
           onReady={handleReady}

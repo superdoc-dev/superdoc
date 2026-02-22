d0881fbc66be5ca28800077f6b703b2d9d85df5e
Thomerson Roncally Araújo Teixeira <thomersonroncally@outlook.com>
2026-02-22 17:56:43 -0400

    docs: add getting started nuxt.js example

---
+++ examples/getting-started/nuxt/app/pages/index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const file = ref<File | null>(null);
+
+const handleFile = (event: Event) => {
+  const input = event.target as HTMLInputElement;
+
+  const selected = input.files?.[0];
+
+  if (selected) {
+    file.value = selected;
+  }
+}
+</script>
+
+<template>
+  <div>
+    <input
+      type="file"
+      accept=".docx"
+      @change="handleFile"
+    >
+    <ClientOnly>
+      <DocEditor
+        v-if="file"
+        :document="file"
+        :user="{ name: 'Vue User', email: 'user@company.com' }"
+      />
+    </ClientOnly>
+  </div>
+</template>

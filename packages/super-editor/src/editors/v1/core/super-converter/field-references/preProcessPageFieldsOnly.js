023859bdb1d9c789bb0eb3b38abdbb455940722e
Luccas Correa <luccas@harbourshare.com>
2026-06-01 15:11:52 -0300

    fix(super-converter): trust header footer field keyword

--- packages/super-editor/src/editors/v1/core/super-converter/field-references/preProcessPageFieldsOnly.js
+++ packages/super-editor/src/editors/v1/core/super-converter/field-references/preProcessPageFieldsOnly.js
@@ -226,7 +226,7 @@ function scanFieldSequence(nodes, beginIndex) {
  * @returns {Function | null}
  */
 function getHeaderFooterFieldPreprocessor(fieldType) {
-  switch (extractFieldKeyword(fieldType)) {
+  switch (fieldType) {
     case 'PAGE':
       return preProcessPageInstruction;
     case 'NUMPAGES':
